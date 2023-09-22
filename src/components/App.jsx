4a7318f9f855e440ee864ee6835a9174c4303908
arhip14 <kruvdyk1503@gmail.com>
2023-09-22 18:21:18 +0300

    bug-fixed-6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+// Компонент App.jsx
 import React, { useEffect } from 'react';
-import { useDispatch} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'; // Додайте useSelector
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import { AppContainer, Title, ContactListContainer} from './AppStyled';
+import { AppContainer, Title, ContactListContainer } from './AppStyled';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { fetchContacts } from '../Redux/actions/contactsActions';
 
 const App = () => {
   const dispatch = useDispatch();
-  // const contacts = useSelector((state) => state.contacts.items);
+  const contacts = useSelector((state) => state.contacts.items); // Додайте використання useSelector
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -22,7 +23,8 @@ const App = () => {
       <ContactForm />
       <Filter />
       <ContactListContainer>
-        <ContactList />
+        {/* Переконайтеся, що contacts існують перед передачею їх у ContactList */}
+        {contacts && <ContactList />}
       </ContactListContainer>
       <ToastContainer autoClose={3000} position={toast.POSITION.TOP_CENTER} />
     </AppContainer>
